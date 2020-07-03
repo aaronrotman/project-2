d47e6c72d1776e50bfeb09e9d61980ab745b6634
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-07-02 20:38:50 -0700

    Create database schema

---
+++ schema.sql
@@ -0,0 +1,25 @@
+-- Drop the ';breweries tabl eif it exists
+DROP TABLE IF EXISTS breweries;
+
+-- Create the 'breweries' table
+CREATE TABLE breweries (
+	key SERIAL PRIMARY KEY,
+	id VARCHAR,
+	name VARCHAR,
+	brewery_type VARCHAR,
+	street VARCHAR,
+	city VARCHAR,
+	state VARCHAR,
+	postal_code VARCHAR,
+	website_url VARCHAR,
+	phone VARCHAR,
+	created_at VARCHAR,
+	updated_at VARCHAR,
+	country VARCHAR,
+	longitude FLOAT,
+	latitude FLOAT,
+	tags VARCHAR
+);
+
+-- Confirm the table was created properly
+SELECT name FROM breweries;
